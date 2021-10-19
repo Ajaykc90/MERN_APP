e7af00e8ffc6ca6f802fee37158bd0f9c8c911c1
Ajay KC <ajaykc90@gmail.com>
2021-10-19 18:48:38 +1100

    updating-readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Notifications from './components/Notifications';
+import Popup from './components/Popup';
+import { showNotifications as show } from './helpers/helpers';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const words = ['application', 'programming', 'interface', 'wizard', 'nightmare', 'australia', 'happy', 'english', 'funny', 'bread', 'pneumonia', 'naruto', 'kangaroo'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const App = () => {
+    const [playable, setPlayable] = useState(true);
+    const [correctLetters, setCorrectLetters] = useState([]);
+    const [wrongLetters, setWrongLetters] = useState([]);
+    const [showNotification, setShowNotification] = useState(false);
+    useEffect(() => {
+        const handlekeydown = event => {
+            const { key, keyCode } = event;
+            if (playable && keyCode >= 65 && keyCode <= 95) {
+                const letter = key.toLowerCase();
+                if (selectedWord.includes(letter)) {
+                    if (!correctLetters.includes(letter)) {
+                        setCorrectLetters((currentLetter) => [...currentLetter, letter])
+                    } else {
+                        show(setShowNotification)
+                    }
+
+                } else {
+                    if (!selectedWord.includes(letter)) {
+                        setWrongLetters((currentLetter) => [...currentLetter, letter])
+                    } else {
+                        show(setShowNotification)
+                    }
+
+                }
+            }
+
+        }
+        window.addEventListener('keydown', handlekeydown);
+        return () => window.removeEventListener('keydown', handlekeydown);
+
+    }, [playable, correctLetters, wrongLetters])
+
+    const playAgain = () => {
+        setPlayable(true);
+        setWrongLetters([]);
+        setCorrectLetters([]);
+        const random = Math.floor(Math.random() * words.length);
+        selectedWord = words[random];
+
+    }
+    return (
+        <>
+            <Header />
+            <div className="game-container">
+                <Figure wrongLetters={wrongLetters} />
+                <WrongLetters wrongLetters={wrongLetters} />
+                <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+            </div>
+            <Popup selectedWord={selectedWord} correctLetters={correctLetters} wrongLetters={wrongLetters} setPlayable={setPlayable} playable={playable} playAgain={playAgain} />
+            <Notifications showNotification={showNotification} />
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>HangMan Game</h1>
+            <p>Find the Hidden word-Enter a letter</p>
+        </div>
+    )
+}
+
+export default Header
